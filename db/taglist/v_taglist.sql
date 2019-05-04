cf01b2dd46f0b5aff5509cea9478d78cbc0d2514
ikosten <ivanitsky@stavropol.ws>
2019-05-04 18:27:22 +0300

    Add view with list of all tags 'v_taglist'

---
+++ db/taglist/v_taglist.sql
@@ -0,0 +1,10 @@
+﻿-- View: v_taglist
+
+-- DROP VIEW v_taglist;
+
+CREATE OR REPLACE VIEW v_taglist AS
+ SELECT tl.id AS nid, tl.code AS scode, tl.name AS sname
+   FROM taglist tl;
+
+ALTER TABLE v_taglist
+  OWNER TO innoreport;
