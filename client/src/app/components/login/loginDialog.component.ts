4bd425a74adc347769b9cf82825c1837400ffbe0
Stefan Popov <stefanpopov@list.ru>
2019-05-05 01:28:54 +0300

    Modify login dialog

--- client/src/app/components/login/loginDialog.component.ts
+++ client/src/app/components/login/loginDialog.component.ts
@@ -1,35 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { AuthService } from '../../services/auth.service';
+import { FormService } from '../../services/form.service';
+import { CustomValidators } from '../shared/dialog/inputValidator';
 
 @Component({
     selector: 'app-login-dialog',
     templateUrl: 'loginDialog.component.html',
     styleUrls: ['loginDialog.component.scss']
 })
-export class LoginDialogComponent {
-
-    private username: string;
-    private password: string;
+export class LoginDialogComponent implements OnInit {
 
     private dialogTitle = 'Login Dialog';
     private submitButtonName = 'Log in';
     private hide = true;
 
+    private loginFormGroup: FormGroup;
+    private formErrors = {
+        name: '',
+        password: ''
+    };
+
     constructor(public dialogRef: MatDialogRef<LoginDialogComponent>,
+        private form: FormBuilder,
+        private readonly formService: FormService,
         private readonly router: Router,
         private readonly authService: AuthService) {
     }
 
+    ngOnInit(): void {
+        this.buildForm();
+    }
+
     public validateUser(): void {
-        this.authService.login(this.username, this.password).then(isLogin => {
-            if (isLogin) {
-                this.dialogRef.close();
-                this.router.navigate(['/reports']);
-                this.username = '';
-                this.password = '';
-            }
+        this.formService.markFormGroupTouched(this.loginFormGroup);
+        if (this.loginFormGroup.valid) {
+            const username: string = this.loginFormGroup.get('name').value;
+            const password: string = this.loginFormGroup.get('password').value;
+            this.authService.login(username, password).then(isLogin => {
+                if (isLogin) {
+                    this.dialogRef.close();
+                    this.router.navigate(['/reports']);
+                }
+            });
+            this.loginFormGroup.reset();
+        } else {
+            this.formErrors = this.formService.validateForm(this.loginFormGroup, this.formErrors, false);
+        }
+    }
+
+    private buildForm(): void {
+        this.loginFormGroup = this.form.group({
+            name: [
+                '', [
+                    Validators.required,
+                    Validators.minLength(5),
+                    Validators.maxLength(20),
+                    CustomValidators.validateCharacters
+                ]
+            ],
+            password: [
+                '', [
+                    Validators.required,
+                    Validators.minLength(5),
+                    Validators.maxLength(20)
+                ]
+            ]
+        });
+
+        this.loginFormGroup.valueChanges.subscribe((data) => {
+            this.formErrors = this.formService.validateForm(this.loginFormGroup, this.formErrors, true);
         });
     }
 }
--- client/src/app/components/login/loginDialog.module.ts
+++ client/src/app/components/login/loginDialog.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { MatDialogRef } from '@angular/material/dialog';
 
 import { LoginDialogComponent } from './loginDialog.component';
 import { MaterialModule } from '../shared/material.module';
 import { DialogModule } from '../shared/dialog/dialog.module';
 import { AuthService } from '../../services/auth.service';
+import { FormService } from '../../services/form.service';
 
 @NgModule({
     declarations: [LoginDialogComponent],
     imports: [
         FormsModule,
         MaterialModule,
-        DialogModule
+        CommonModule,
+        DialogModule,
+        ReactiveFormsModule
     ],
     bootstrap: [LoginDialogComponent],
     providers: [
         {provide: MatDialogRef, useValue: {}},
-        AuthService
+        AuthService,
+        FormService
     ]
 })
 export class LoginDialogModule {}
