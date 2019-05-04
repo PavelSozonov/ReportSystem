f81c8ea233c3d6208c250e283bdbd69041ea1350
ikosten <ivanitsky@stavropol.ws>
2019-05-04 19:38:38 +0300

    Add field 'sentity' to view 'v_reporttags'

--- db/reports/reporttags/v_reporttags.sql
+++ db/reports/reporttags/v_reporttags.sql
@@ -4,10 +4,11 @@
 
 CREATE OR REPLACE VIEW v_reporttags AS 
  SELECT rt.id AS nid, rt.report AS nreport, r.number AS snumber, 
-    tl.code AS scode
+    tl.code AS scode, el.code as sentity
    FROM reporttags rt
    JOIN reports r ON r.id = rt.report
-   JOIN taglist tl ON rt.tag = tl.id;
+   JOIN taglist tl ON rt.tag = tl.id
+   LEFT JOIN entitylist el ON el.id = tl.entity;
 
 ALTER TABLE v_reporttags
   OWNER TO innoreport;
